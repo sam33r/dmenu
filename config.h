30d176ec4fff3b7ca93059bac587798e8fb30542
Sameer <samahuja@google.com>
2020-09-11 11:57:34 -0700

    Some more visual tweaks.

--- config.h
+++ config.h
@@ -4,15 +4,15 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Iosevka Sparkle:size=11"
+	"Iosevka Sparkle:size=14"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *dynamic     = NULL;      /* -dy option; dynamic command to run on input change */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#333", "#fff" },
+	[SchemeNorm] = { "#333", "#ffffff" },
 	[SchemeSel] = { "#000", "#eee" },
-	[SchemeSelHighlight] = { "#6e0117", "#eee" },
+	[SchemeSelHighlight] = { "#d9002b", "#fff7ba" },
 	[SchemeNormHighlight] = { "#d9002b", "#fff7ba" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
