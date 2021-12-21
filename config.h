2a3e7728e70979fc368781994afe16778bc1f8df
Sameer Ahuja <sameer.ahuja@gmail.com>
2021-12-21 18:58:29 +0530

    Switch fonts.

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 0;                       /* -F option; if 0, doesn't use fuzzy matching */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Iosevka Sparkle:size=14"
+	"Iosevka Aile:size=14"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *dynamic     = NULL;      /* -dy option; dynamic command to run on input change */
